97ddd361bcf50fe0a3c63618b62b22b94395b97a
ecojnr <thomasstratton6@gmail.com>
2022-11-15 14:30:55 +0000

    controller edit

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 
-const models = require("../be-nc-games/controllers/games.js");
+const controllers = require("../be-nc-games/controllers/games.js");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/api/reviews", models.getReviews);
-app.get("/api/categories", models.getCategories);
+app.get("/api/reviews", controllers.getReviews);
+app.get("/api/categories", controllers.getCategories);
 
 app.all("/*", (req, res) => {
 	res.status(404).send({ msg: "Route not found" });
